086b5616072638a491f73ff5ee40d623fb83ecb4
angel.garcia <angel.garcia@students.salle.url.edu>
2023-04-15 10:55:29 +0200

    Initial commit, made the structure

---
+++ code/LSControlTower.X/main.c
@@ -0,0 +1,16 @@
+#include <pic18f4321.h>
+#include <xc.h>
+
+#pragma config OSC = INTIO2
+#pragma config PBADEN = DIG
+#pragma config WDT = OFF
+
+
+void main(void) {
+    /************************
+     * INIT OSC (16MHz)
+     ***********************/
+    OSCCON = 0b01100000;
+    OSCTUNEbits.PLLEN = 1;
+    return;
+}
