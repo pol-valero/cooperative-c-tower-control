615605578817a27056a1ac6050111e663da45b85
angel.garcia <angel.garcia@students.salle.url.edu>
2023-04-30 19:49:10 +0200

    Fixed 2 time called motor

--- code/LSControlTower.X/main.c
+++ code/LSControlTower.X/main.c
@@ -86,7 +86,6 @@ void main(void) {
         motorLCD();
         motorCurrentTime();
         motorModifyTime();
-        motorModifyTime();
         motorRecord();
         motorNote();
         motorMelody();
